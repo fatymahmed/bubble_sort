20b8359ab8734ae794e55a3ba2e70e9a0a6070e2
Urchmaney <kingsobino@gmail.com>
2019-08-06 21:49:36 +0000

    Finish with the enumerable instructions.

--- enumarable.rb
+++ enumarable.rb
@@ -24,54 +24,65 @@ module Enumerable
     def my_all
         flag=true
         self.my_each{ |element| flag=flag && yield(element)}
-        puts flag
+        flag
     end  
 
     def my_any
       flag=false
       self.my_each{ |element| flag= flag || yield(element)}
-      puts flag
+      flag
     end  
 
     def my_none
        flag=false
         self.my_each{ |element| flag=flag || yield(element)}
-        puts !flag
+        !flag
     end  
 
     def my_count
       count=0   
       self.my_each{ |element| count+=1 if yield(element)}
-      puts count
+      count
    end  
 
-   def my_map
-    result=[]
-    self.my_each{ |element| result << yield(element)}
-    puts result
-  end  
+    def my_map(proc = nil)
+      result=[]
+      self.my_each{ |element|
+        if proc
+          result <<  proc.call(element)         
+        else
+          result << yield(element)
+        end
+      }
+      result
+    end  
 
   def my_inject(i)
     res=i||0
     self.my_each{ |element| res=yield(res,element)}
-    puts res
+    res
   end
 
   end
 
-def multiply_els(arr)
-    arr.my_inject(1){|res,ele| res*ele}
-end
+# def multiply_els(arr)
+#     arr.my_inject(1){|res,ele| res*ele}
+# end
 
-  arr=[1,2,3,4]
-  arr.my_each{|element| puts element}
-  arr.my_each_with_index{|index,element| puts "index: #{index} for #{element}"}
-  [2,2,2,4,8].my_all { |num|  num.even?  }
-  [2,2,2,4,8].my_any { |num|  num.odd?  }
-  [2,2,2,5,7].my_none { |num|  num.odd?  }
+#   arr=[1,2,3,4]
+#   arr.my_each{|element| puts element}
+#   arr.my_each_with_index{|index,element| puts "index: #{index} for #{element}"}
+#   [2,2,2,4,8].my_all { |num|  num.even?  }
+#   [2,2,2,4,8].my_any { |num|  num.odd?  }
+#   [2,2,2,5,7].my_none { |num|  num.odd?  }
 
-  [2,2,2,5,2].my_count{ |num|  num.odd?  }
-  ["hi","hey"].my_map{|element| element.upcase}
-  [2,2,2,5,2].my_inject(1){|res,ele| res+ele}
-  multiply_els([2,4,5])
+#   [2,2,2,5,2].my_count{ |num|  num.odd?  }
+#   ["hi","hey"].my_map{|element| element.upcase}
+#   [2,2,2,5,2].my_inject(1){|res,ele| res+ele}
+#   multiply_els([2,4,5])
   
+#   proc=Proc.new{
+#     |element| 
+#     element.upcase
+#   }
+#   ["hi","hey"].my_map(proc)
