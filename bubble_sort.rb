fca581e7443f60321a099a86743ac826c25b15e3
Urchmaney <kingsobino@gmail.com>
2019-08-07 23:18:29 +0000

    Linter : Empty line after guard clause

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -4,7 +4,9 @@ def bubble_sort(arr)
   arr.each do
     arr.each_with_index do |_, index|
       next if index == arr.length - 1
+
       next unless arr[index] > arr[index + 1]
+
       tmp = arr[index]
       arr[index] = arr[index + 1]
       arr[index + 1] = tmp
@@ -16,7 +18,9 @@ def bubble_sort_by(array)
   array.each do
     array.each_with_index do |_, index|
       next if index == array.length - 1
+
       next unless yield(array[index], array[index + 1]).positive?
+      
       temp = array[index]
       array[index] = array[index + 1]
       array[index + 1] = temp
