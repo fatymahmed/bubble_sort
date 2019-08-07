8aadceb05eadbabf3a2c61812a7bbec7c536d7ce
Urchmaney <kingsobino@gmail.com>
2019-08-07 15:46:39 +0000

    Add space to operation in Ln 8 and uncommment  test for bubble_sort_by.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -5,7 +5,7 @@ def bubble_sort(arr)
     if arr[index] > arr[index + 1]        
       tmp = arr[index]
       arr[index] = arr[index + 1]
-      arr[index+1] = tmp
+      arr[index + 1] = tmp
     end
     end
     }
@@ -25,9 +25,11 @@ def bubble_sort_by (array)
   end
 end
 
-bubble_sort_by(["hi","hello","hey"]) do |left,right|
+arr=["hi","hello","hey"]
+bubble_sort_by(arr) do |left,right|
   left.length - right.length
 end
+puts arr
 
 # arr=[4,3,78,2,0,2]
 
